57f282175f1e8ec769f899fe7f66152b81bf22b8
PunchlY <punchly@foxmail.com>
2024-05-28 04:23:44 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import build from '@hono/vite-cloudflare-pages'
-import devServer from '@hono/vite-dev-server'
-import adapter from '@hono/vite-dev-server/cloudflare'
-import { defineConfig } from 'vite'
+import build from '@hono/vite-cloudflare-pages';
+import devServer from '@hono/vite-dev-server';
+import adapter from '@hono/vite-dev-server/cloudflare';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
-    build(),
+    build({
+      entry: 'src/index.ts'
+    }),
     devServer({
       adapter,
       entry: 'src/index.ts'
     })
   ]
-})
+});

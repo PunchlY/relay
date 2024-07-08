ea69c2827e9036740cfd71c8d7e52892903cddc8
PunchlY <punchly@foxmail.com>
2024-07-09 06:28:23 +0800

    -

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import { Hono } from 'hono';
+import { cache } from 'hono/cache';
 
 const app = new Hono();
 
-app.get('/age.tv/m3u8/:play', async (c) => {
-    const { play } = c.req.param();
-    const url = new URL(`https://43.240.156.118:8443/m3u8`);
-    url.searchParams.set('url', play);
-    const res = await fetch(url);
-    const text = await res.text();
-    const playUrl = text.match(/var Vurl = ('|")(.+?)\1/)![2];
-    if (import.meta.env.DEV)
-        return c.text('play ' + playUrl);
-    return c.redirect(playUrl);
+app.get('*', cache({
+    cacheName: 'my-app',
+    cacheControl: 'max-age=604800',
+}));
+
+app.get('/', async (c) => {
+    return c.text('hello.');
 });
 
 app.get('/:protocol{http(s?):}//img.hellogithub.com/*', async (c) => {
@@ -33,6 +31,14 @@ app.get('/:protocol{http(s?):}//:host{.+\\.sinaimg\\.cn}/*', async (c) => {
             Referer: 'https://weibo.com/',
         },
     });
+    return c.newResponse(await res.arrayBuffer(), {
+        headers: res.headers,
+    });
+});
+
+app.get('/:protocol{http(s?):}//img-blog.csdnimg.cn/*', async (c) => {
+    const url = c.req.path.substring(1);
+    const res = await fetch(url);
     return c.newResponse(await res.arrayBuffer(), {
         headers: res.headers,
     });

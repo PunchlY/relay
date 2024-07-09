a5e8273353a709a1e1109a171673d6a916d9a83a
PunchlY <punchly@foxmail.com>
2024-07-09 16:42:50 +0800

    -

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,3 +5,10 @@ declare module 'hono' {
     (content: string | Promise<string>, props?: { title?: string; }): Response;
   }
 }
+
+declare global {
+  function fetch(
+    input: RequestInfo,
+    init?: RequestInit<RequestInitCfProperties>,
+  ): Promise<Response>;
+}
--- src/index.ts
+++ src/index.ts
@@ -13,35 +13,23 @@ app.get('/', async (c) => {
 });
 
 app.get('/:protocol{http(s?):}//img.hellogithub.com/*', async (c) => {
-    const url = c.req.path.substring(1);
-    const res = await fetch(url, {
+    return fetch(c.req.path.substring(1), {
         headers: {
             Referer: 'https://hellogithub.com/',
         },
     });
-    return c.newResponse(await res.arrayBuffer(), {
-        headers: res.headers,
-    });
 });
 
 app.get('/:protocol{http(s?):}//:host{.+\\.sinaimg\\.cn}/*', async (c) => {
-    const url = c.req.path.substring(1);
-    const res = await fetch(url, {
+    return fetch(c.req.path.substring(1), {
         headers: {
             Referer: 'https://weibo.com/',
         },
     });
-    return c.newResponse(await res.arrayBuffer(), {
-        headers: res.headers,
-    });
 });
 
 app.get('/:protocol{http(s?):}//img-blog.csdnimg.cn/*', async (c) => {
-    const url = c.req.path.substring(1);
-    const res = await fetch(url);
-    return c.newResponse(await res.arrayBuffer(), {
-        headers: res.headers,
-    });
+    return fetch(c.req.path.substring(1));
 });
 
 app.get('/:protocol{http(s?):}//:host/*', (c) => {

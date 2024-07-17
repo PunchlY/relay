28d70e697d71732593f887a8d553d0ff7252e469
PunchlY <punchly@foxmail.com>
2024-07-18 02:36:20 +0800

    -

--- src/http.ts
+++ src/http.ts
@@ -1,5 +1,5 @@
 import { Hono } from 'hono';
-import type { Context } from 'hono';
+import { createMiddleware } from 'hono/factory';
 import type { StatusCode } from 'hono/utils/http-status';
 
 const app = new Hono<{
@@ -8,59 +8,40 @@ const app = new Hono<{
     };
 }>().basePath(':protocol{http(s?):}//');
 
-app.use(async (c, next) => {
-    switch (c.req.method) {
-        case 'GET':
-        case 'HEAD': break;
-        default: return c.status(405);
-    }
+app.get(async (c, next) => {
     c.set('url', c.req.url.substring(c.req.url.indexOf('/', 8) + 1));
     await next();
 });
 
-app.use('img.hellogithub.com/*', async (c) => {
-    return $fetch(c, c.var.url, {
-        headers: { Referer: 'https://hellogithub.com/' },
+app.get('img.hellogithub.com/*', $fetch({
+    headers: { Referer: 'https://hellogithub.com/' },
+}));
+
+app.get(':host{.+\\.sinaimg\\.cn}/*', $fetch({
+    headers: { Referer: 'https://weibo.com/' },
+}));
+
+app.get($fetch());
+
+function $fetch(init?: RequestInit<RequestInitCfProperties>) {
+    return createMiddleware<{
+        Variables: {
+            url: string;
+        };
+    }>(async (c) => {
+        const _headers = new Headers(init?.headers);
+
+        const range = c.req.header('Range');
+        range && _headers.set('Range', range);
+
+        const { body, status, headers } = await fetch(c.var.url, {
+            ...init,
+            headers: _headers,
+            method: c.req.method,
+        });
+        const newHeaders = Object.fromEntries(headers.entries());
+        return c.newResponse(body as ReadableStream, status as StatusCode, newHeaders);
     });
-});
-
-app.use(':host{.+\\.sinaimg\\.cn}/*', async (c) => {
-    return $fetch(c, c.var.url, {
-        headers: { Referer: 'https://weibo.com/' },
-    });
-});
-
-app.use(':host{.+\\.csdnimg\\.cn}/*', async (c) => {
-    return $fetch(c, c.var.url, {
-        headers: { Referer: 'https://blog.csdn.net/' },
-    });
-});
-
-app.use('developer.qcloudimg.com/*', async (c) => {
-    return $fetch(c, c.var.url, {
-        headers: { Referer: 'https://cloud.tencent.com/' },
-    });
-});
-
-app.use('*', async (c) => {
-    if (import.meta.env.DEV)
-        return c.text('Back: ' + c.var.url);
-    return c.redirect(c.var.url);
-});
-
-async function $fetch(c: Context, input: RequestInfo, init?: RequestInit<RequestInitCfProperties>) {
-    const _headers = new Headers(init?.headers);
-
-    const range = c.req.header('Range');
-    range && _headers.set('Range', range);
-
-    const { body, status, headers } = await fetch(input, {
-        ...init,
-        headers: _headers,
-        method: c.req.method,
-    });
-    const newHeaders = Object.fromEntries(headers.entries());
-    return c.newResponse(body as ReadableStream, status as StatusCode, newHeaders);
 }
 
 export default app;

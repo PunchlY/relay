86034dbffded2b979b956b9d07708c415150e1dc
PunchlY <punchly@foxmail.com>
2024-07-18 02:08:53 +0800

    -

--- src/http.ts
+++ src/http.ts
@@ -10,42 +10,56 @@ const app = new Hono<{
 }>().basePath(':protocol{http(s?):}//');
 
 app.use(async (c, next) => {
+    switch (c.req.method) {
+        case 'GET':
+        case 'HEAD': break;
+        default: return;
+    }
     c.set('url', c.req.url.substring(c.req.url.indexOf('/', 8) + 1));
     await next();
 });
 
-app.get('img.hellogithub.com/*', async (c) => {
+app.use('img.hellogithub.com/*', async (c) => {
     return $fetch(c, c.var.url, {
         headers: { Referer: 'https://hellogithub.com/' },
     });
 });
 
-app.get(':host{.+\\.sinaimg\\.cn}/*', async (c) => {
+app.use(':host{.+\\.sinaimg\\.cn}/*', async (c) => {
     return $fetch(c, c.var.url, {
         headers: { Referer: 'https://weibo.com/' },
     });
 });
 
-app.get('img-blog.csdnimg.cn/*', async (c) => {
+app.use(':host{.+\\.csdnimg\\.cn}/*', async (c) => {
     return $fetch(c, c.var.url, {
         headers: { Referer: 'https://blog.csdn.net/' },
     });
 });
 
-app.get('developer.qcloudimg.com/*', async (c) => {
+app.use('developer.qcloudimg.com/*', async (c) => {
     return $fetch(c, c.var.url, {
         headers: { Referer: 'https://cloud.tencent.com/' },
     });
 });
 
-app.get(':host/*', (c) => {
+app.use('*', async (c) => {
     if (import.meta.env.DEV)
         return c.text('Back: ' + c.var.url);
     return c.redirect(c.var.url);
 });
 
 async function $fetch(c: Context, input: RequestInfo, init?: RequestInit<RequestInitCfProperties>) {
-    const { body, status, headers } = await fetch(input, init);
+    const _headers = new Headers(init?.headers);
+
+    const range = c.req.header('Range');
+    range && _headers.set('Range', range);
+
+    const { body, status, headers } = await fetch(input, {
+        ...init,
+        headers: _headers,
+        method: c.req.method,
+    });
     const newHeaders = Object.fromEntries(headers.entries());
     return c.newResponse(body as ReadableStream, status as StatusCode, newHeaders);
 }

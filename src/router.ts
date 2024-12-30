71bba7b062ca57b183f85fa3a9e9493861cc1339
PunchlY <punchly@foxmail.com>
2024-12-30 13:30:00 +0800

    -

--- src/router.ts
+++ src/router.ts
@@ -11,7 +11,7 @@ type Method = typeof Method[number];
 const AsyncGeneratorFunction = (async function* () { }).constructor as AsyncGeneratorFunctionConstructor;
 const GeneratorFunction = (function* () { }).constructor as GeneratorFunctionConstructor;
 
-function newResponse(response: Response, init?: ResponseInit): Response;
+function newResponse(response: Response): Response;
 function newResponse(stream?: (...args: any[]) => AsyncGenerator, init?: ResponseInit): Response;
 function newResponse(body: BodyInit | null, init?: ResponseInit): Response;
 function newResponse(data: number | bigint | string | object, init?: ResponseInit): Response;
@@ -23,7 +23,7 @@ function newResponse(data: unknown, init?: ResponseInit) {
             return new Response(data, init);
         case 'object':
             if (data instanceof Response)
-                return new Response(data.body, { ...data, ...init });
+                return data;
             if (data === null || data instanceof Blob || data instanceof ReadableStream || data instanceof FormData || data instanceof ArrayBuffer || ArrayBuffer.isView(data) || data instanceof URLSearchParams || data instanceof FormData)
                 return new Response(data, init);
             break;

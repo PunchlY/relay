e932ef2ebbf306f248998f3bd6a694ba41c9e689
PunchlY <punchly9lin@gmail.com>
2025-09-24 07:55:38 +0800

    add xget

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,45 @@
 import { PatternRouter } from "./router";
+// @ts-ignore
+import xget from "xget/src/index.js";
 
 interface Env {
-    // ASSETS: Fetcher;
+  // ASSETS: Fetcher;
 }
 
 const proxyRouter = new PatternRouter<Env>()
-    // .addRoute("GET", "https://img.hellogithub.com", ({ request }) => {
-    //     request.headers.set('Referer', 'https://hellogithub.com/');
-    //     return fetch(request);
-    // })
-    // .addRoute("GET", "https://*.sinaimg.cn", ({ request }) => {
-    //     request.headers.set('Referer', 'https://weibo.com/');
-    //     return fetch(request);
-    // })
-    // .addRoute("GET", "https://*.csdnimg.cn", ({ request }) => {
-    //     request.headers.set('Referer', 'https://csdn.net/');
-    //     return fetch(request);
-    // })
-    .addRoute(({ request }) => {
-        request.headers.set('Referer', request.url);
-        return fetch(request);
-    });
+  // .addRoute("GET", "https://img.hellogithub.com", ({ request }) => {
+  //     request.headers.set('Referer', 'https://hellogithub.com/');
+  //     return fetch(request);
+  // })
+  // .addRoute("GET", "https://*.sinaimg.cn", ({ request }) => {
+  //     request.headers.set('Referer', 'https://weibo.com/');
+  //     return fetch(request);
+  // })
+  // .addRoute("GET", "https://*.csdnimg.cn", ({ request }) => {
+  //     request.headers.set('Referer', 'https://csdn.net/');
+  //     return fetch(request);
+  // })
+  .addRoute(({ request }) => {
+    request.headers.set('Referer', request.url);
+    return fetch(request);
+  });
 
 const router = new PatternRouter<Env>()
-    .addRoute("GET", { pathname: "/" }, () => {
-        return new Response("hello");
-    })
-    .addRoute({ pathname: "/(https?://.+)" }, ({ request, env, ctx, pattern }) => {
-        let url = pattern.pathname.groups[0];
-        const search = pattern.search.input;
-        if (search) {
-            url += `?${search}`;
-        }
-        return proxyRouter.fetch(new Request(url, request), env, ctx);
-    });
+  .addRoute("GET", { pathname: "/" }, () => {
+    return new Response("hello");
+  })
+  .addRoute({ pathname: "/(https?://.+)" }, ({ request, env, ctx, pattern }) => {
+    let url = pattern.pathname.groups[0]!;
+    const search = pattern.search.input;
+    if (search) {
+      url += `?${search}`;
+    }
+    return proxyRouter.fetch(new Request(url, request), env, ctx);
+  })
+  .addRoute(({ request, env, ctx }) => {
+    return xget.fetch(request, env, ctx);
+  });
 
 export default {
-    fetch: router.fetch,
+  fetch: router.fetch,
 } satisfies ExportedHandler<Env>;
